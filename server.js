c45d5e1c23e97ffdc2435d0919c7908bb03d3e1f
satanas <wil.alejandro@gmail.com>
2015-12-05 11:15:25 -0300

    Start and joining match from server

--- server.js
+++ server.js
@@ -19,69 +19,81 @@ var matches = [];
 
 var server = require('socket.io')(app);
 server.on('connection', function(socket) {
+  console.log('connectingaS:dA_S.dasñkdas');
   socket.on('new-match', function() {
+    console.log('new match bitch');
     matches.push(new Match(server, socket));
+    matches[0].create();
   });
 
-  if (Object.keys(players).length === 0) {
-    players[socket.id] = new Player(socket);
-    battlefield[socket.id] = {};
-    socket.emit('joined', {
-      'player': {
-        'id': socket.id,
-        'health': players[socket.id].health
-      }
-    });
-  } else if (Object.keys(players).length === 1) {
-    players[socket.id] = new Player(socket);
-    battlefield[socket.id] = {};
-    socket.emit('joined', {
-      'player': {
-        'id': socket.id,
-        'health': players[socket.id].health
-      }
-    });
-    server.emit('ready');
-    console.log('Ready!');
-    turnOrder = Object.keys(players);
-    console.log('ble 1', turnOrder);
-    turnOrder.shuffle();
-    console.log('ble 2', turnOrder);
+  socket.on('join-match', function(matchId) {
+    var match = matches[0];
+    match.joinPlayer();
+    if (Object.keys(match.players).length > 1) {
+      match.start();
+      performTurn();
+    }
+  });
 
-    // Send hands to users
-    Object.keys(players).forEach(function(key) {
-      var opponentKeys = _.filter(Object.keys(players), function(k) {
-        return (k !== key);
-      });
-      var opponents = [];
-      opponentKeys.forEach(function(key) {
-        opponents.push({
-          'player': {
-            'id': key,
-            'health': players[key].health
-          }
-        });
-      });
-      players[key].socket.emit('opponents', opponents);
-      var hands = {
-        you: {
-          hand: players[key].hand
-        }
-      }
-      opponents.forEach(function(o) {
-        hands[o.player.id] = {
-          count: players[o.player.id].hand.length
-        }
-      });
-      players[key].socket.emit('hands', hands);
-    });
+  //if (Object.keys(players).length === 0) {
+  //  players[socket.id] = new Player(socket);
+  //  battlefield[socket.id] = {};
+  //  socket.emit('joined', {
+  //    'player': {
+  //      'id': socket.id,
+  //      'health': players[socket.id].health
+  //    }
+  //  });
+  //} else if (Object.keys(players).length === 1) {
+  //  players[socket.id] = new Player(socket);
+  //  battlefield[socket.id] = {};
+  //  socket.emit('joined', {
+  //    'player': {
+  //      'id': socket.id,
+  //      'health': players[socket.id].health
+  //    }
+  //  });
+  //  server.emit('ready');
+  //  console.log('Ready!');
+  //  turnOrder = Object.keys(players);
+  //  console.log('ble 1', turnOrder);
+  //  turnOrder.shuffle();
+  //  console.log('ble 2', turnOrder);
 
-    performTurn();
-  } else {
-    socket.emit('joined', {'player': {'id': null}});
-    socket.emit('rejected', 'Battle started');
-    console.log('Connection refused, battle started');
-  }
+  //  // Send hands to users
+  //  Object.keys(players).forEach(function(key) {
+  //    var opponentKeys = _.filter(Object.keys(players), function(k) {
+  //      return (k !== key);
+  //    });
+  //    var opponents = [];
+  //    opponentKeys.forEach(function(key) {
+  //      opponents.push({
+  //        'player': {
+  //          'id': key,
+  //          'health': players[key].health
+  //        }
+  //      });
+  //    });
+  //    players[key].socket.emit('opponents', opponents);
+  //    var hands = {
+  //      you: {
+  //        hand: players[key].hand
+  //      }
+  //    }
+  //    opponents.forEach(function(o) {
+  //      hands[o.player.id] = {
+  //        count: players[o.player.id].hand.length
+  //      }
+  //    });
+  //    players[key].socket.emit('hands', hands);
+  //  });
+
+  //  performTurn();
+  //} else {
+  //  socket.emit('joined', {'player': {'id': null}});
+  //  socket.emit('rejected', 'Battle started');
+  //  console.log('Connection refused, battle started');
+  //}
 
   socket.on('draw', function(cardId) {
     if (!isPlayerInTurn(socket.id)) return socket.emit('no-turn');

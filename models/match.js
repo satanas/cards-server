2a20957924a48a196c73ac64a8e8c81706585873
satanas <wil.alejandro@gmail.com>
2015-12-05 11:14:57 -0300

    Adding method to start the match

--- models/match.js
+++ models/match.js
@@ -16,10 +16,10 @@ Match.prototype.create = function() {
   this.joinPlayer();
 
   console.log('Match ' + this.id + ' created');
-  socket.emit('match-created', this.id);
+  this.socket.emit('match-created', this.id);
 };
 
-Math.prototype.joinPlayer = function() {
+Match.prototype.joinPlayer = function() {
   this.players[this.socket.id] = new Player(this.socket);
   this.battlefield[this.socket.id] = {};
   this.socket.emit('joined', {
@@ -27,6 +27,43 @@ Math.prototype.joinPlayer = function() {
       'id': this.socket.id,
       'health': this.players[this.socket.id].health
     }
+  });
+};
+
+Match.prototype.start = function() {
+  this.server.emit('ready');
+  console.log('Ready!');
+  this.turnOrder = Object.keys(this.players);
+  console.log('ble 1', this.turnOrder);
+  this.turnOrder.shuffle();
+  console.log('ble 2', this.turnOrder);
+
+  // Send hands to users
+  Object.keys(this.players).forEach(function(key) {
+    var opponentKeys = _.filter(Object.keys(this.players), function(k) {
+      return (k !== key);
+    });
+    var opponents = [];
+    opponentKeys.forEach(function(key) {
+      opponents.push({
+        'player': {
+          'id': key,
+          'health': this.players[key].health
+        }
+      });
+    });
+    this.players[key].socket.emit('opponents', opponents);
+    var hands = {
+      you: {
+        hand: this.players[key].hand
+      }
+    }
+    opponents.forEach(function(o) {
+      hands[o.player.id] = {
+        count: this.players[o.player.id].hand.length
+      }
+    });
+    this.players[key].socket.emit('hands', hands);
   });
 };
 

f7a4038142559312d5e1b1f1281f674e95b7096d
satanas <wil.alejandro@gmail.com>
2015-12-16 01:48:36 -0300

    Added more cards with venom

--- models/deck.js
+++ models/deck.js
@@ -6,7 +6,7 @@ var Deck = function(playerId) {
   cardStorage = [
     new Card(1, global.cardTypes.CREATURE, 1, 1, 'Bla Monster', 'image.jpg', 1, 'Description'),
     new Card(2, global.cardTypes.CREATURE, 1, 2, 'Ble Monster', 'image.jpg', 1, 'Description'),
-    new Card(3, global.cardTypes.CREATURE, 2, 2, 'Bli Monster', 'image.jpg', 1, 'Description'),
+    new Card(3, global.cardTypes.CREATURE, 2, 2, 'Bli Monster', 'image.jpg', 1, 'Description', {venom: true}),
     new Card(4, global.cardTypes.CREATURE, 4, 4, 'Blo Monster', 'image.jpg', 2, 'Description'),
     new Card(5, global.cardTypes.CREATURE, 1, 4, 'Blu Monster', 'image.jpg', 2, 'Description', {transfusion: true}),
     new Card(6, global.cardTypes.CREATURE, 1, 1, 'Kla Monster', 'image.jpg', 3, 'Description'),

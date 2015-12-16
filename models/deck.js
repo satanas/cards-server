6c859d429d6697999215d3d4c24ce2237649178a
satanas <wil.alejandro@gmail.com>
2015-12-16 11:00:37 -0300

    Refactored global import

--- models/deck.js
+++ models/deck.js
@@ -1,25 +1,25 @@
 var Card = require('./card');
-var global = require('../global');
+var Global = require('../global');
 
 var Deck = function(playerId) {
   // Read the cards storage and create a 60 cards deck
   cardStorage = [
-    new Card(1, global.cardTypes.CREATURE, 1, 1, 'Bla Monster', 'image.jpg', 1, 'Description'),
-    new Card(2, global.cardTypes.CREATURE, 1, 2, 'Ble Monster', 'image.jpg', 1, 'Description'),
-    new Card(3, global.cardTypes.CREATURE, 2, 2, 'Bli Monster', 'image.jpg', 1, 'Description', {venom: true}),
-    new Card(4, global.cardTypes.CREATURE, 4, 4, 'Blo Monster', 'image.jpg', 2, 'Description'),
-    new Card(5, global.cardTypes.CREATURE, 1, 4, 'Blu Monster', 'image.jpg', 2, 'Description', {transfusion: true}),
-    new Card(6, global.cardTypes.CREATURE, 1, 1, 'Kla Monster', 'image.jpg', 3, 'Description'),
-    new Card(7, global.cardTypes.CREATURE, 1, 2, 'Klev Monster', 'image.jpg', 3, 'Description', {venom: true}),
-    new Card(8, global.cardTypes.CREATURE, 2, 2, 'Kli Monster', 'image.jpg', 4, 'Description'),
-    new Card(9, global.cardTypes.CREATURE, 4, 4, 'Klo Monster', 'image.jpg', 4, 'Description'),
-    new Card(10, global.cardTypes.CREATURE, 1, 4, 'Klu Monster', 'image.jpg', 5, 'Description', {transfusion: true}),
-    new Card(11, global.cardTypes.CREATURE, 2, 3, 'Fla Monster', 'image.jpg', 5, 'Description'),
-    new Card(12, global.cardTypes.CREATURE, 1, 1, 'Hurried Monster', 'image.jpg', 2, 'Description', {rush: true}),
-    new Card(13, global.cardTypes.CREATURE, 3, 2, 'Powerful Lad', 'image.jpg', 3, 'Description', {overwhelm: true}),
-    new Card(14, global.cardTypes.CREATURE, 1, 1, 'Spearman', 'image.jpg', 2, 'Description', {firstStrike: true}),
-    new Card(15, global.cardTypes.CREATURE, 2, 2, 'Master Sword', 'image.jpg', 3, 'Description', {firstStrike: true}),
-    new Card(16, global.cardTypes.CREATURE, 1, 1, 'Damned', 'image.jpg', 2, 'Description', {deathtouch: true}),
+    new Card(1, Global.cardTypes.CREATURE, 1, 1, 'Bla Monster', 'image.jpg', 1, 'Description'),
+    new Card(2, Global.cardTypes.CREATURE, 1, 2, 'Ble Monster', 'image.jpg', 1, 'Description'),
+    new Card(3, Global.cardTypes.CREATURE, 2, 2, 'Bli Monster', 'image.jpg', 1, 'Description', {venom: true}),
+    new Card(4, Global.cardTypes.CREATURE, 4, 4, 'Blo Monster', 'image.jpg', 2, 'Description'),
+    new Card(5, Global.cardTypes.CREATURE, 1, 4, 'Blu Monster', 'image.jpg', 2, 'Description', {transfusion: true}),
+    new Card(6, Global.cardTypes.CREATURE, 1, 1, 'Kla Monster', 'image.jpg', 3, 'Description'),
+    new Card(7, Global.cardTypes.CREATURE, 1, 2, 'Klev Monster', 'image.jpg', 3, 'Description', {venom: true}),
+    new Card(8, Global.cardTypes.CREATURE, 2, 2, 'Kli Monster', 'image.jpg', 4, 'Description'),
+    new Card(9, Global.cardTypes.CREATURE, 4, 4, 'Klo Monster', 'image.jpg', 4, 'Description'),
+    new Card(10, Global.cardTypes.CREATURE, 1, 4, 'Klu Monster', 'image.jpg', 5, 'Description', {transfusion: true}),
+    new Card(11, Global.cardTypes.CREATURE, 2, 3, 'Fla Monster', 'image.jpg', 5, 'Description'),
+    new Card(12, Global.cardTypes.CREATURE, 1, 1, 'Hurried Monster', 'image.jpg', 2, 'Description', {rush: true}),
+    new Card(13, Global.cardTypes.CREATURE, 3, 2, 'Powerful Lad', 'image.jpg', 3, 'Description', {overwhelm: true}),
+    new Card(14, Global.cardTypes.CREATURE, 1, 1, 'Spearman', 'image.jpg', 2, 'Description', {firstStrike: true}),
+    new Card(15, Global.cardTypes.CREATURE, 2, 2, 'Master Sword', 'image.jpg', 3, 'Description', {firstStrike: true}),
+    new Card(16, Global.cardTypes.CREATURE, 1, 1, 'Damned', 'image.jpg', 2, 'Description', {deathtouch: true}),
   ];
   this.playerId = playerId;
   this.cards = cardStorage.slice(0);

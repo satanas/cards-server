c6de6b0e8f9ba57ad3e61afb1868b659abea23a4
satanas <wil.alejandro@gmail.com>
2016-01-09 01:13:36 -0300

    More collections to populate

--- scripts/populate_db.js
+++ scripts/populate_db.js
@@ -8,6 +8,10 @@ MongoClient.connect('mongodb://127.0.0.1:27017/magic', function(err, db) {
   populateSingleValues(db, ["n/a", "player", "card", "self", "hand", "any"], 'target_types');
   populateSingleValues(db, ["n/a", "undamaged", "damaged", "class", "any"], 'target_conditions');
   populateSingleValues(db, ["n/a", "friend", "foe", "any"], 'target_bands');
+  populateSingleValues(db, ["no", "yes", "random"], 'target_selects');
+  populateSingleValues(db, ["play", "turn", "damage_received", "damage_dealt", "dead"], 'events');
+  populateSingleValues(db, ["n/a", "health", "attack"], 'mod_attributes');
+  populateSingleValues(db, ["n/a", "add", "replace", "mult"], 'mod_operations');
 
   db.close();
 });

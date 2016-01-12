3cfcf0a4e5ad3387d95f3046ef582437b2d556a7
satanas <wil.alejandro@gmail.com>
2016-01-11 22:47:29 -0300

    Added backbone model for modifications

---
+++ public/js/frontend/models/modification.js
@@ -0,0 +1,25 @@
+var ModificationModel = Backbone.Model.extend({
+  defaults: {
+    spell: '',
+    attribute: '',
+    operation: '',
+    ability: '',
+    multiplier: '',
+    value: ''
+  },
+  validate: function() {
+    $.ajax('/modifications/validate', {
+      data: JSON.stringify(this.toJSON()),
+      method: 'POST',
+      processData: false,
+      contentType: false,
+      context: this,
+      success: function(data) {
+        this.trigger('validated', null, data.responseJSON);
+      },
+      error: function(data) {
+        this.trigger('validated', data.responseJSON.errors);
+      }
+    });
+  }
+});

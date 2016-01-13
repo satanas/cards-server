a6e36bd03535561e1f5b05a0d6a157d20a5940ae
satanas <wil.alejandro@gmail.com>
2016-01-13 14:10:49 -0300

    Removed backbone updates from enchantment form view

--- public/js/frontend/views/enchantment_form.js
+++ public/js/frontend/views/enchantment_form.js
@@ -2,26 +2,8 @@ var EnchantmentFormView = Backbone.View.extend({
   el: '#enchantment-div',
   initialize: function(options) {
     this.template = Handlebars.compile($('#enchantment-form-template').html());
-    this.model.on('change', this.onChange, this);
     this.model.on('modifications-updated', this.renderModifications, this);
     this.render();
-
-    //$('[data-belongs-to="enchantment"]').on('keyup', function(ev) {
-    //  console.log(this.model);
-    //  this.model.update($('#enchantment_form').serializeObject());
-    //  console.log('up', this.model.toJSON());
-    //}.call(this));
-  },
-  onChange: function() {
-    this.render();
-    $('#add_modification_button').on('click',addModification);
-    $('#add_enchantment_button').on('click', addEnchantment);
-
-    //$('[data-belongs-to="enchantment"]').on('keyup', function(ev) {
-    //  console.log(this.model);
-    //  this.model.update($('#enchantment_form').serializeObject());
-    //  console.log('up', this.model.toJSON());
-    //}.call(this));
   },
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));

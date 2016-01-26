4e0070a6715c6b2aa3b3b744aa5ad0cd0790ea55
satanas <wil.alejandro@gmail.com>
2016-01-26 14:27:41 -0300

    Removed validation code from enchantment

--- models/enchantment.js
+++ models/enchantment.js
@@ -18,27 +18,4 @@ var schema = mongoose.Schema({
 
 var Enchantment = mongoose.model('enchantments', schema);
 
-Enchantment.validate = function(enchantment) {
-  var errors = [];
-
-  var mustBePresent = function(obj, attr) {
-    if (attr instanceof Array) {
-      attr.forEach(function(e) {
-        if (obj[e] === '') errors.push({field: e, message: `${e} can not be empty`});
-      });
-    } else {
-      if (obj[attr] === '') errors.push({field: attr, message: `${attr} can not be empty`});
-    }
-  };
-
-  if (enchantment.description === '') errors.push({field: 'description', message: 'description can not be empty'});
-  if (enchantment.event === '') errors.push({field: 'event', message: 'event can not be empty'});
-
-  if (enchantment.target.select === 'yes') {
-    mustBePresent(enchantment.target, ['type', 'quantity']);
-  }
-
-  return errors;
-};
-
 module.exports = Enchantment;

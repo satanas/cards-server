22a2bc7f8a12e72bb117a28f0fa39d96426b1e2d
satanas <wil.alejandro@gmail.com>
2016-01-26 01:01:29 -0300

    Updated frontend validator

--- public/js/frontend/models/validator.js
+++ public/js/frontend/models/validator.js
@@ -1,12 +1,12 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Validator = function() {
-  console.log('see you later, alligatori kakkakakakaka');
   this.errors = [];
+  this.genericErrorId = 'generic-error';
 };
 
 Validator.prototype.constructor = Validator;
 
-Validator.prototype.mustBePresent = function(obj, attr) {
+Validator.prototype._mustBePresent = function(obj, attr) {
   if (attr instanceof Array) {
     attr.forEach(function(e) {
       if (obj[e] === '') this.errors.push({field: e, message: e + " can not be empty"});
@@ -16,7 +16,7 @@ Validator.prototype.mustBePresent = function(obj, attr) {
   }
 };
 
-Validator.prototype.mustBeAbsent = function(obj, attr) {
+Validator.prototype._mustBeAbsent = function(obj, attr) {
   if (attr instanceof Array) {
     attr.forEach(function(e) {
       if (obj[e] !== '') this.errors.push({field: e, message: "spell '" + obj.spell + "' can not have " + e});
@@ -26,33 +26,63 @@ Validator.prototype.mustBeAbsent = function(obj, attr) {
   }
 };
 
-Validator.prototype.validateModifications = function(mod) {
+var ModificationValidator = function() {
+  Validator.call(this);
+};
+
+ModificationValidator.prototype = Object.create(Validator.prototype);
+ModificationValidator.prototype.constructor = ModificationValidator;
+
+ModificationValidator.prototype.validate = function(mod) {
   this.errors = [];
+
   // If you're adding a modification, it has to have a spell
   if (mod.spell === '') this.errors.push({field: 'spell', message: 'spell can not be empty'});
 
   if (mod.spell === 'attribute') {
-    this.mustBePresent(mod, ['attribute', 'operation', 'value']);
-    this.mustBeAbsent(mod, 'ability');
+    this._mustBePresent(mod, ['attribute', 'operation', 'value']);
+    this._mustBeAbsent(mod, 'ability');
   }
   if (mod.spell === 'ability') {
-    this.mustBePresent(mod, 'ability');
-    this.mustBeAbsent(mod, ['attribute', 'operation', 'multiplier', 'value']);
+    this._mustBePresent(mod, 'ability');
+    this._mustBeAbsent(mod, ['attribute', 'operation', 'multiplier', 'value']);
   }
   if (mod.spell === 'draw' || mod.spell === 'damage' || mod.spell === 'summon' || mod.spell === 'transform' ||
      mod.spell === 'necromancy' || mod.spell === 'player_health' || mod.spell === 'protect_player' ||
      mod.spell === 'mana_cost') {
-    this.mustBePresent(mod, 'value');
-    this.mustBeAbsent(mod, ['attribute', 'operation', 'ability', 'multiplier']);
+    this._mustBePresent(mod, 'value');
+    this._mustBeAbsent(mod, ['attribute', 'operation', 'ability', 'multiplier']);
   }
   if (mod.spell === 'protect_card' || mod.spell === 'disenchant' || mod.spell === 'return' ||
       mod.spell === 'kill_target' || mod.spell === 'freeze') {
-    this.mustBeAbsent(mod, ['attribute', 'operation', 'ability', 'multiplier', 'value']);
+    this._mustBeAbsent(mod, ['attribute', 'operation', 'ability', 'multiplier', 'value']);
+  }
+
+  return this.errors;
+};
+
+ModificationValidator.prototype.compare = function(obj1, obj2) {
+  this.errors = [];
+  if (obj1.spell === 'draw' && obj1.spell === obj2.spell) return true;
+  if (obj1.spell === 'damage' && obj1.spell === obj2.spell) return true;
+  if (obj1.spell === 'transform' && obj1.spell === obj2.spell) return true;
+  if (obj1.spell === 'protect_card' && obj1.spell === obj2.spell) return true;
+  if (obj1.spell === '' && obj1.spell === obj2.spell) return true;
+
+  var equal = true;
+  for (var attr in obj1) {
+    equal = equal && obj2[attr] === obj1[attr];
+  }
+  if (equal) {
+    this.errors.push({field: this.genericErrorId, message: 'you can not add duplicated spells'});
   }
   return this.errors;
 };
 
-module.exports = Validator;
+module.exports = {
+  Modifications: ModificationValidator,
+  Enchantments: null
+};
 
 },{}]},{},[1])(1)
 });

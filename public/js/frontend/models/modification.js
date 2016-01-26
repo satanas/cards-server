e535346af8490995855291cab018d7393e5bc4f8
satanas <wil.alejandro@gmail.com>
2016-01-26 00:42:19 -0300

    Removed modification validations from backbone model

--- public/js/frontend/models/modification.js
+++ public/js/frontend/models/modification.js
@@ -6,29 +6,5 @@ var ModificationModel = Backbone.Model.extend({
     ability: '',
     multiplier: '',
     value: ''
-  },
-  validate: function() {
-    $.ajax('/modifications/validate', {
-      data: JSON.stringify(this.toJSON()),
-      method: 'POST',
-      processData: false,
-      contentType: false,
-      context: this,
-      success: function(data) {
-        this.trigger('validated', null, data);
-      },
-      error: function(data) {
-        this.trigger('validated', data.responseJSON.errors);
-      }
-    });
-  },
-  compare: function(obj) {
-    if (obj.spell === 'draw' && obj.spell === this.get('spell')) return true;
-
-    var equal = true;
-    for (var attr in obj.attributes) {
-      equal = equal && this.get(attr) === obj.get(attr);
-    }
-    return equal;
   }
 });
